22ddc2f1eabbc25b656ff2bc34cf5db2f306bffd
Abdelrhman Said <119136719+AbdlrhmanSaid@users.noreply.github.com>
2025-02-28 11:17:11 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import AzkarContainer from "./components/AzkarContainer"
-import Verse from "./components/Verse"
-import VerseAzkar from "./components/VerseAzkar"
-import VerseText from "./components/VerseText"
+import Navbaar from "./components/Navbar";
 
 function App() {
-
   return (
-      <AzkarContainer isNight={false}>
-        <VerseAzkar sura="Al-Fatiha" verse={1}>
-          <VerseText>
-            بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
-          </VerseText>
-        </VerseAzkar>
-        <Verse sura="Al-Fatiha" verse={1}>
-          <VerseText>
-            بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
-          </VerseText>
-        </Verse>
-      </AzkarContainer>
-  )
+    <>
+      <Navbaar />
+    </>
+  );
 }
 
-export default App
+export default App;
