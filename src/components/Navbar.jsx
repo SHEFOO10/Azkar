1892c28cb8aa6d0b6d2efad9ac728ff464f4957e
Abdelrhman Said <119136719+AbdlrhmanSaid@users.noreply.github.com>
2025-03-07 13:24:41 +0200

    create mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,41 +6,71 @@ import { IoMenu } from "react-icons/io5";
 const navs = ["Prayer Times", "Azkar", "Duas", "Prayer Guide", "Calendar"];
 
 const Navbar = () => {
-  const [theme, setTheme] = useState("")
+  const [theme, setTheme] = useState("");
+  const [isOpen, setISOpen] = useState(false);
   useLayoutEffect(() => {
-    const html = document.documentElement
+    const html = document.documentElement;
     html.classList.toggle(
-      'dark',
-      localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
-    )
-  }, [theme])
+      "dark",
+      localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+  }, [theme]);
   const changeTheme = () => {
-    const html = document.documentElement
-    html.classList.toggle('dark')
-  }
+    const html = document.documentElement;
+    html.classList.toggle("dark");
+  };
   return (
-    <nav className="flex items-center justify-around p-5">
-      <div className="text-primary-dark dark:text-primary-light font-bold text-lg">IslamicPrayers</div>
-      <ul className="gap-6 flex text-gray-700">
-        {navs.map((nav, index) => (
-          <li
-            key={index}
-            className="cursor-pointer hidden md:block text-[#4B5563] dark:text-gray-300 font-semibold"
-          >
-            {nav}
-          </li>
-        ))}
-        <li>
-          <IoMenu className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300 block md:hidden" />
-        </li>
-        <li>
-          <FiSearch className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300" />
-        </li>
-        <li>
-          <MdOutlineNightlight className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300" onClick={changeTheme} />
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <div className="main-nav flex items-center justify-around p-5">
+          <div className="text-primary-dark dark:text-primary-light font-bold text-lg">
+            IslamicPrayers
+          </div>
+          <ul className="gap-6 flex text-gray-700">
+            {navs.map((nav, index) => (
+              <li
+                key={index}
+                className="cursor-pointer hidden md:block text-[#4B5563] dark:text-gray-300 font-semibold"
+              >
+                {nav}
+              </li>
+            ))}
+            <li
+              onClick={() => {
+                setISOpen(!isOpen);
+              }}
+            >
+              <IoMenu className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300 block md:hidden" />
+            </li>
+            <li>
+              <FiSearch className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300" />
+            </li>
+            <li>
+              <MdOutlineNightlight
+                className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300"
+                onClick={changeTheme}
+              />
+            </li>
+          </ul>
+        </div>
+        {isOpen && (
+          <div className="mobile-nav flex items-center  p-5 dark:bg-[#0F1521] bg-[#E2DDD8]  md:hidden">
+            <ul className="gap-3 flex flex-col text-gray-700">
+              {navs.map((nav, index) => (
+                <li
+                  key={index}
+                  className="cursor-pointer  text-[#4B5563] dark:text-gray-300 font-semibold"
+                >
+                  {nav}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
