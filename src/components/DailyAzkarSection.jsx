ae2a8b0391c713836c784498d9fc431ff0828c6f
Abdelrhman Said <119136719+AbdlrhmanSaid@users.noreply.github.com>
2025-03-09 21:42:24 +0200

    added text when azkar done

--- src/components/DailyAzkarSection.jsx
+++ src/components/DailyAzkarSection.jsx
@@ -2,66 +2,89 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { BsArrowRepeat } from "react-icons/bs";
 import { FaArrowRotateRight } from "react-icons/fa6";
+import { FaRegCheckCircle } from "react-icons/fa";
 
-const DailyAzkarSection = ({ title, icon, colorOne, colorTwo, azkar: _azkar }) => {
-  const { t, i18n } = useTranslation()
-  const content = t('daily_azkar', { returnObjects: true })
-  const isArabic = i18n.language === 'ar'
-  const [azkar, setAzkar] = useState(_azkar)
+const DailyAzkarSection = ({
+  title,
+  icon,
+  colorOne,
+  colorTwo,
+  azkar: _azkar,
+}) => {
+  const { t, i18n } = useTranslation();
+  const content = t("daily_azkar", { returnObjects: true });
+  const isArabic = i18n.language === "ar";
+  const [azkar, setAzkar] = useState(_azkar);
 
   const handleRepeation = (i) => {
-    const zikr = azkar[i]
+    const zikr = azkar[i];
     if (zikr.repeat === 1) {
-      setAzkar([
-        ...azkar.slice(0, i),
-        ...azkar.slice(i + 1)
-      ])
+      setAzkar([...azkar.slice(0, i), ...azkar.slice(i + 1)]);
     } else {
       setAzkar([
         ...azkar.slice(0, i),
         {
           ...zikr,
-          repeat: zikr.repeat - 1
-
+          repeat: zikr.repeat - 1,
         },
-        ...azkar.slice(i + 1)
-      ])
+        ...azkar.slice(i + 1),
+      ]);
     }
-  }
+  };
   return (
-    <div className="w-full max-w-md h-fit dark:bg-gray-800 shadow-lg rounded-2xl overflow-hidden" dir={isArabic ? 'rtl' : "ltr"}>
+    <div
+      className="w-full max-w-md h-fit dark:bg-gray-800 shadow-lg rounded-2xl overflow-hidden"
+      dir={isArabic ? "rtl" : "ltr"}
+    >
       <div
         className="flex items-center justify-between px-4 py-3 text-white"
         style={{
           background: `linear-gradient(to right, ${colorOne}, ${colorTwo})`,
         }}
       >
-        <div className='flex gap-2 items-center'>
+        <div className="flex gap-2 items-center">
           <span className="text-xl mr-2">{icon}</span>
           <h1 className="text-lg font-semibold">{title}</h1>
         </div>
-        <button className='cursor-pointer' onClick={() => setAzkar(_azkar)}>
+        <button className="cursor-pointer" onClick={() => setAzkar(_azkar)}>
           <FaArrowRotateRight />
         </button>
       </div>
+      {azkar.length === 0 ? (
+        <p className="text-center text-lg font-medium dark:text-gray-400 p-4 flex items-center justify-center gap-2">
+          <span>تَقَبَّلَ اللَّهُ</span>
+          <span className={`text-primary-dark dark:text-primary-light`}>
+            <FaRegCheckCircle />
+          </span>
+        </p>
+      ) : (
+        <div
+          className={`flex flex-col ${azkar.length === 0 ? "" : "p-4"} gap-4`}
+        >
+          {azkar.map((item, index) => (
+            <div
+              key={index}
+              className="cursor-pointer dark:bg-gray-700 dark:text-white p-4 rounded-xl shadow-sm transition hover:scale-105"
+              onClick={() => handleRepeation(index)}
+            >
+              <h1 className="text-right text-lg font-semibold mb-2">
+                {item.arabic}
+              </h1>
+              <p className="text-sm dark:text-gray-400 mb-3" dir={"ltr"}>
+                {item.translation}
+              </p>
 
-      <div className={`flex flex-col ${azkar.length === 0 ? "" : 'p-4'} gap-4`}>
-        {azkar.map((item, index) => (
-          <div key={index} className="cursor-pointer dark:bg-gray-700 dark:text-white p-4 rounded-xl shadow-sm transition hover:scale-105" onClick={() => handleRepeation(index)}>
-            <h1 className="text-right text-lg font-semibold mb-2">
-              {item.arabic}
-            </h1>
-            <p className="text-sm dark:text-gray-400 mb-3" dir={'ltr'}>{item.translation}</p>
-
-            <div className="flex justify-between items-center text-sm font-medium">
-              <div className="text-primary-dark dark:text-primary-light flex items-center gap-1">
-                <BsArrowRepeat />
-                {content.repeat} {item.repeat} {item.repeat > 1 ? content.times : content.time}
+              <div className="flex justify-between items-center text-sm font-medium">
+                <div className="text-primary-dark dark:text-primary-light flex items-center gap-1">
+                  <BsArrowRepeat />
+                  {content.repeat} {item.repeat}{" "}
+                  {item.repeat > 1 ? content.times : content.time}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,113 +1,71 @@
+import "./styles/Navbar.css";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { MdOutlineNightlight } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
 import { useTranslation } from "react-i18next";
 
 const navs = [
-  { name: "navbar.prayer_times", id: 'prayer-times' },
-  { name: "navbar.azkar", id: 'daily-azkar' },
-  { name: "navbar.duas", id: 'duas' },
-  { name: "navbar.prayer_guide", id: 'prayer-guide' },
-  { name: "navbar.calendar", id: 'islamic-calendar' },
+  { name: "navbar.prayer_times", id: "prayer-times" },
+  { name: "navbar.azkar", id: "daily-azkar" },
+  { name: "navbar.duas", id: "duas" },
+  { name: "navbar.prayer_guide", id: "prayer-guide" },
+  { name: "navbar.calendar", id: "islamic-calendar" },
 ];
 
-const Navbar = () => {
-  const [theme, setTheme] = useState("");
-  const [isOpen, setISOpen] = useState(false);
-  useLayoutEffect(() => {
-    const html = document.documentElement;
-    html.classList.toggle(
-      "dark",
-      localStorage.theme === "dark" ||
-        (!("theme" in localStorage) &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches)
-    );
-  }, [theme]);
-  const changeTheme = () => {
-    const html = document.documentElement;
-    html.classList.toggle("dark");
-  };
+const Navbar = ({ changeTheme }) => {
+  const { t } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <nav>
-        <div className="main-nav flex items-center justify-around p-5">
-          <div className="text-primary-dark dark:text-primary-light font-bold text-lg">
-            IslamicPrayers
-          </div>
-          <ul className="gap-6 flex text-gray-700">
-            {navs.map((nav, index) => (
-              <li
-                key={index}
-                className="cursor-pointer hidden md:block text-[#4B5563] dark:text-gray-300 font-semibold"
-              >
-                {nav}
-              </li>
-            ))}
-            <li
-              onClick={() => {
-                setISOpen(!isOpen);
-              }}
-            >
-              <IoMenu className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300 block md:hidden" />
-            </li>
-            <li>
-              <FiSearch className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300" />
-            </li>
-            <li>
-              <MdOutlineNightlight
-                className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300"
-                onClick={changeTheme}
-              />
-            </li>
-          </ul>
+      {/* main nav */}
+      <nav className="flex items-center justify-around p-5">
+        <div className="flex items-center text-primary-dark dark:text-primary-light font-bold text-lg">
+          <img width={64} height={64} src="/ITCLUB.png" /> {t("navbar.title")}
         </div>
-        {isOpen && (
-          <div className="mobile-nav flex items-center  p-5 dark:bg-[#0F1521] bg-[#E2DDD8]  md:hidden">
-            <ul className="gap-3 flex flex-col text-gray-700">
-              {navs.map((nav, index) => (
-                <li
-                  key={index}
-                  className="cursor-pointer  text-[#4B5563] dark:text-gray-300 font-semibold"
-                >
-                  {nav}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </nav>
-    </>
-=======
-const Navbar = ({ changeTheme }) => {
-  const { t } = useTranslation()
-  return (
-    <nav className="flex items-center justify-around p-5">
-      <div className="flex  items-center text-primary-dark dark:text-primary-light font-bold text-lg"><img width={64} height={64} src="/ITCLUB.png" /> {t('navbar.title')}</div>
-      <ul className="gap-6 flex text-gray-700">
-        {navs.map(({ name, id }, index) => (
-          <li
-            key={index}
-            className="cursor-pointer hidden md:block text-[#4B5563] dark:text-gray-300 font-semibold"
-          >
-            <a
-              href={`#${id}`}
+        <ul className="gap-6 flex text-gray-700">
+          {navs.map(({ name, id }, index) => (
+            <li
+              key={index}
+              className="cursor-pointer hidden md:block text-[#4B5563] dark:text-gray-300 font-semibold"
             >
-              {t(name)}
-            </a>
+              <a href={`#${id}`}>{t(name)}</a>
+            </li>
+          ))}
+          <li onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <IoMenu className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300 block md:hidden" />
           </li>
-        ))}
-        <li>
-          <IoMenu className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300 block md:hidden" />
-        </li>
-        {/* <li>
+          {/* <li>
           <FiSearch className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300" />
         </li> */}
-        <li>
-          <MdOutlineNightlight className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300" onClick={() => changeTheme(e => e === 'dark' ? 'light' : 'dark')} />
-        </li>
-      </ul>
-    </nav>
->>>>>>> bdbf64c112ed634be60ad83fc0de374adec63395
+          <li>
+            <MdOutlineNightlight
+              className="text-xl cursor-pointer text-[#4B5563] dark:text-gray-300"
+              onClick={() =>
+                changeTheme((e) => (e === "dark" ? "light" : "dark"))
+              }
+            />
+          </li>
+        </ul>
+      </nav>
+      {/* mobile nav */}
+      {isMenuOpen && (
+        <ul
+          className={`text-gray-700 px-[50px] py-5 block md:hidden bg-[#FAFAF9] dark:bg-[#1A202C] 
+    rounded-lg shadow-lg transition-all 
+    ${isMenuOpen ? "menu-enter" : "menu-exit"}`}
+        >
+          {navs.map(({ name, id }, index) => (
+            <li
+              key={index}
+              className="cursor-pointer my-3 text-[#4B5563] dark:text-gray-300 font-semibold transition-all duration-300 hover:translate-x-1"
+            >
+              <a href={`#${id}`}>{t(name)}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
